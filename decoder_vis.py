859db30149adc8a35fb2fecd910fdb4b536df95f
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-01-16 15:16:26 +0000

    created DecodedViewerWrapper

--- decoder_vis.py
+++ decoder_vis.py
@@ -1,4 +1,6 @@
-from inspect_agent import load_policy, latest_model_path
+from common.env.custom_viewer_wrapper import DecodedViewerWrapper
+from helper import print_values_actions
+from inspect_agent import load_policy, latest_model_path, predict
 import torch
 from common.model import Decoder
 
@@ -12,7 +14,7 @@ def main(args):
 
     # load encoder
     action_names, done, env, hidden_state, obs, policy = load_policy(render=False, logdir=encoder_path, n_envs=2)
-
+    encoder = policy.embedder
     # load decoder
     decoder = Decoder(
         embedding_dim=32,
@@ -24,6 +26,16 @@ def main(args):
     last_model = latest_model_path(decoder_path)
     decoder.load_state_dict(torch.load(last_model, map_location=device)["model_state_dict"])
 
+    env = DecodedViewerWrapper(env, encoder, decoder, info_key="rgb")
+    while True:
+        act, log_prob_act, value, next_hidden_state, pi = predict(policy, obs, hidden_state, done)
+        print_values_actions(action_names, pi, value)
+        next_obs, rew, done, info = env.step(act)
+        # rewards = np.append(rewards, rew[done])
+        obs = next_obs
+        hidden_state = next_hidden_state
+        if done[0]:
+            print(f"Level seed: {info[0]['level_seed']}")
     # load data
     # process example
     # print example
