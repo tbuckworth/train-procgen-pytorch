4274675777957e9ce159e02f4bcf8892c8997b71
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-02-06 12:56:38 +0000

    registering nan_hooks

--- train.py
+++ train.py
@@ -233,9 +233,7 @@ def train_ppo(args):
                     print("In", self.__class__.__name__)
                     raise RuntimeError(f"Found NAN in output {i} at indices: ", nan_mask.nonzero(), "where:",
                                        out[nan_mask.nonzero()[:, 0].unique(sorted=True)])
-            else:
-                print("In", self.__class__.__name__)
-                print("Found NoneType when searching for NANs")
+
 
     if args.detect_nan:
         for submodule in model.modules():
