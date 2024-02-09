77a36373602af9b6a4dd9b28c8f85f46b337ef50
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-02-09 10:56:28 +0000

    fixed timeout logic in logger

--- train_hparams.py
+++ train_hparams.py
@@ -47,13 +47,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-
-
     args.exp_name = "coinrun-hparams"
     args.env_name = "coinrun"
     args.distribution_mode = "hard"
     args.param_name = "hard-500-impalafsqmha"
-    args.num_timesteps = 2**20
+    args.num_timesteps = 2 * 2 ** 20
     args.num_checkpoints = 1
     args.seed = 6033
     args.num_levels = 10
@@ -62,15 +60,26 @@ if __name__ == '__main__':
     args.wandb_tags = ["n_envs", "n_minibatches", "big_bottleneck", "better_info"]
     args.device = "gpu"
     args.use_valid_env = False
-    args.minibatches = [8, 4, 2]
-    for n_envs in [2, 256, 128, 64, 32, 16]:
-        for n_steps in [256, 128, 64]:
-            for n_minibatch in args.minibatches:
-                args.n_envs = n_envs
-                args.n_steps = n_steps
-                args.n_minibatch = n_minibatch
-                args.wandb_name = f"{args.n_envs}x{args.n_steps}_{args.n_minibatch}"
-                train_ppo(args)
+
+    n_envs_steps_minib = [[16, 256, 4],
+                          [16, 256, 2],
+                          [32, 64, 2],
+                          [32, 64, 2],
+                          [32, 256, 8],
+                          [64, 64, 2],
+                          [64, 128, 8],
+                          [64, 128, 2],
+                          [64, 256, 4],
+                          [256, 64, 2],
+                          [256, 256, 8],
+                          [64, 128, 8]]
+
+    for n_envs, n_steps, n_minibatch in n_envs_steps_minib:
+        args.n_envs = n_envs
+        args.n_steps = n_steps
+        args.n_minibatch = n_minibatch
+        args.wandb_name = f"{args.n_envs}x{args.n_steps}_{args.n_minibatch}"
+        train_ppo(args)
 
     # for n_envs in [256, 128, 64, 32, 16]:
     #     for n_steps in [256, 128, 64]:
@@ -80,3 +89,4 @@ if __name__ == '__main__':
     #             args.n_minibatch = n_minibatch
     #             args.wandb_name = f"{args.n_envs}x{args.n_steps}_{args.n_minibatch}"
     #             train_ppo(args)
+
