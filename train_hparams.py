e0c87025c56cbcce82259c6163860e5e07d71385
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-03-25 16:00:14 +0000

    trying cat instead

--- train_hparams.py
+++ train_hparams.py
@@ -22,14 +22,14 @@ def executable_train(hparams):
     return '\n'.join(
         ["hn=$(hostname)",
          "echo ${hn} > ${hn}.txt",
-         "export PATH=/vol/bitbucket/${USER}/train-procgen-pytorch/venvproc/bin/:/vol/cuda/12.2.0/bin/:$PATH",
-         "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/vol/cuda/12.2.0/lib64:/vol/cuda/12.2.0/lib",
+         # "export PATH=/vol/bitbucket/${USER}/train-procgen-pytorch/venvproc/bin/:/vol/cuda/12.2.0/bin/:$PATH",
+         # "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/vol/cuda/12.2.0/lib64:/vol/cuda/12.2.0/lib",
          "source /vol/bitbucket/${USER}/train-procgen-pytorch/venvproc/bin/activate",
-         ". /vol/cuda/12.2.0/setup.sh",
-         "TERM=vt100",
-         "/usr/bin/nvidia-smi",
-         "export CUDA_DIR=/vol/cuda/12.2.0/:${CUDAPATH}",
-         "export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/12.2.0/",
+         # ". /vol/cuda/12.2.0/setup.sh",
+         # "TERM=vt100",
+         # "/usr/bin/nvidia-smi",
+         # "export CUDA_DIR=/vol/cuda/12.2.0/:${CUDAPATH}",
+         # "export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/12.2.0/",
          f"python3.8 /vol/bitbucket/${{USER}}/train-procgen-pytorch/train.py {hparams}",
          ])
 
