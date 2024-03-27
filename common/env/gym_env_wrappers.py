ef6caac5ce290e92a542ba283193411834ad1334
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-03-27 10:58:11 +0000

    created cartpole

--- common/env/gym_env_wrappers.py
+++ common/env/gym_env_wrappers.py
@@ -1,7 +1,7 @@
 import gym3
 import gym
 import numpy as np
-from gym3 import ToBaselinesVecEnv
+from gym3 import ToBaselinesVecEnv, ViewerWrapper
 
 from common.env.procgen_wrappers import VecNormalize
 
@@ -10,19 +10,35 @@ class GymWrapper(gym.Wrapper):
     def seed(self, seed):
         return self.reset(seed=seed)
 
-def create_gym_env(env_name="CartPole-v1"):
+def create_baselines_vec_from_gym(env_name="CartPole-v1", num=2):
     # Requires gym==0.25.2 (0.26.2 won't work!)
     def create_cartpole():
         return GymWrapper(gym.make(env_name))
-    envs = gym3.vectorize_gym(num=3, env_fn=create_cartpole)
+    envs = gym3.vectorize_gym(num=num, env_fn=create_cartpole)
     venv = ToBaselinesVecEnv(envs)
     return venv
-    # venv = VecNormalize(venv, ob=False)
 
 
 def gym_test():
-    pass
+    env_args = {"env_name": "CartPole-v1",
+                "n_envs": 2}
+    venv = create_env_gym(env_args, render=False)
 
+
+def create_env_gym(env_args, render, normalize_rew=True):
+    n_envs = env_args["n_envs"]
+    venv = create_baselines_vec_from_gym(env_name=env_args["env_name"], num=n_envs)
+    if render:
+        venv = ViewerWrapper(venv, info_key="rgb")
+    venv = ToBaselinesVecEnv(venv)
+    # TODO: check if we need this:
+    # venv = VecExtractDictObs(venv, "rgb")
+    if normalize_rew:
+        venv = VecNormalize(venv, ob=False)  # normalizing returns, but not
+        # the img frames
+    # venv = TransposeFrame(venv)
+    # venv = ScaledFloatFrame(venv)
+    return venv
 
 if __name__ == "__main__":
     gym_test()
