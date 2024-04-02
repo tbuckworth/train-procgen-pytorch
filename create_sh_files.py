48908949f73848ad555c8664d7d20b0c2352560a
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-04-02 16:43:33 +0100

    modified create_sh_files.py for boxworld hparam search

--- create_sh_files.py
+++ create_sh_files.py
@@ -1,5 +1,6 @@
 import argparse
 import copy
+import itertools
 
 from helper import add_training_args, latest_model_path
 
@@ -18,8 +19,11 @@ def format_args(arg):
                 output += f"--{var_name} {d[var_name]} "
     return output
 
+def executable_python(hparams, name):
+    return f"python3.8 /vol/bitbucket/${{USER}}/train-procgen-pytorch/train.py {hparams} 2>&1 | tee /vol/bitbucket/${{USER}}/train-procgen-pytorch/scripts/train_{name}.out\n"
 
-def executable_train(hparams, name):
+
+def executable_train(hparams, name, python_execs=[]):
     # return f'"hn=$(hostname); echo ${{hn}} > ${{hn}}.txt; cd pyg/train-procgen-pytorch; source venvcartpole/bin/activate; train.py {hparams}"'
 
     return '\n'.join(
@@ -35,13 +39,10 @@ def executable_train(hparams, name):
          "/usr/bin/nvidia-smi",
          "export CUDA_DIR=/vol/cuda/12.2.0/:${CUDAPATH}",
          "export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/12.2.0/",
-         f"python3.8 /vol/bitbucket/${{USER}}/train-procgen-pytorch/train.py {hparams} 2>&1 | tee /vol/bitbucket/${{USER}}/train-procgen-pytorch/scripts/train_{name}.out\n",
-         ])
+         ] + python_execs)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser = add_training_args(parser)
-    args = parser.parse_args()
+
+def add_coinrun_sparsity_params(args):
     args.exp_name = "coinrun-hparams"
     args.env_name = "coinrun"
     args.distribution_mode = "hard"
@@ -57,14 +58,61 @@ if __name__ == '__main__':
     args.use_valid_env = False
     args.n_minibatch = 16
     args.model_file = latest_model_path("logs/train/coinrun/coinrun/2024-03-26__09-11-40__seed_6033")
+    return args
 
-    sparsity = [0.05, 0.075, 0.1, 0.2]
-    arg_list = [copy.deepcopy(args) for _ in sparsity]
-    for arg, sc in zip(arg_list, sparsity):
-        arg.sparsity_coef = sc
-        arg.wandb_name = f"ft_sparse_{sc:.1E}"
-        hparams = format_args(arg)
-        exe = executable_train(hparams, arg.wandb_name)
+
+def add_boxworld_params(args):
+    args.exp_name = "boxworld"
+    args.env_name = "boxworld"
+    args.param_name = "boxworld-ribfsqmha-easy"
+    args.num_timesteps = int(1e7)
+    args.num_checkpoints = 1
+    args.seed = 6033
+    args.use_wandb = True
+    args.wandb_tags = ["h-param", "easy", "gamma", "n_envs", "normalize_rew", "levels", "n_minibatch"]
+    args.device = "gpu"
+    args.use_valid_env = False
+    return args
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser = add_training_args(parser)
+    args = parser.parse_args()
+    n_gpu = 6
+
+    # args = add_coinrun_sparsity_params(args)
+    args = add_boxworld_params(args)
+
+    hparams = {
+        "n_envs": [32, 64, 256],
+        "gamma": [0.99, 0.95, 0.999],
+        "n_minibatch": [8, 16, 32],
+        "normalize_rew": [True, False],
+        "levels": [[10, 10], [8, 5, 5, 5], [9, 9, 9]],
+    }
+    hparam_list = [v for v in itertools.product(*hparams.values())]
+    keys, values = zip(*hparams.items())
+    permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
+
+    arg_list = [copy.deepcopy(args) for _ in hparam_list]
+    n = len(arg_list)//n_gpu
+    for gpu in range(n_gpu):
+        python_execs = []
+        for arg in arg_list[gpu*n:(gpu+1)*n]:
+            for hparam_list in permutations_dicts:
+                nme = ""
+                for key in hparam_list.keys():
+                    v = hparam_list[key]
+                    v_str = v
+                    if type(v) == list:
+                        v_str = ','.join(v)
+                    nme += f"{key}_{v_str}"
+                    arg.__dict__[key] = v
+                arg.wandb_name = nme
+                hparams = format_args(arg)
+                python_execs += executable_python(hparams, arg.wandb_name)
+        exe = executable_train(hparams, arg.wandb_name, python_execs)
         exe_file_name = f"scripts/tmp_file_{arg.wandb_name}.sh"
         f = open(exe_file_name, 'w', newline='\n')
         f.write(exe)
