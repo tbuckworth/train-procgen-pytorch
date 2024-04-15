16086c13e1f8f2b83fc241dde3b68c1502ddf4a4
tbuckworth <elkilesh@hotmail.co.uk>
2024-04-15 14:23:30 +0100

    getting symb reg compatible with boxworld - added weight metrics

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 clear
 source /vol/bitbucket/${USER}/train-procgen-pytorch/venvcartpole/bin/activate
-python3.8 /vol/bitbucket/${USER}/train-procgen-pytorch/create_sh_files.py --n_gpu $1 --execute --hparam_type $2
+python3.8 /vol/bitbucket/${USER}/train-procgen-pytorch/create_sh_files.py --n_gpu $1 --execute --hparam_type $2 --max_runs $3
 deactivate
 ls -tl scripts/tmp* | head -n$1
