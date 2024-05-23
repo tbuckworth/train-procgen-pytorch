ed8a49fba1962628cfedb6491555c1dba50fafbd
tbuckworth <elkilesh@hotmail.co.uk>
2024-05-23 09:25:34 +0100

    setup graph transition hparams

--- create_sh_files.py
+++ create_sh_files.py
@@ -355,35 +355,35 @@ def coinrun_mostlyneural_hparams():
 def cartpole_graph_transition_hparams():
     return {
         "exp_name": [None],
-        "env_name": ['cartpole'],
+        "env_name": ['cartpole', 'cartpole-swing'],
         # "distribution_mode": ['hard'],
         "param_name": ['graph-transition'],
         "device": ["gpu"],
         "num_timesteps": [int(2e6)],
         "seed": [6033],  # 0, 1, 101, 40],
         "gamma": [0.95],  # 0.9],
-        "val_epochs": [1, 2, 3, 5, 8],
-        "dyn_epochs": [1, 2, 3],
+        "val_epochs": [3, 8, 12],
+        "dyn_epochs": [3, 5, 8],
         "learning_rate": [0.00025, 0.0005],
         "t_learning_rate": [0.00025, 0.001],
         "n_envs": [64, 32],
         "n_steps": [256],
-        "n_rollouts": [1, 2, 3],
+        "n_rollouts": [1, 3, 5],
         "temperature": [1, 10, 100, 1000, 100000],
         "use_gae": [True],
-        "rew_coef": [10, 5, 1, 0.1],
+        "rew_coef": [1, 0.1],
         "done_coef": [5., 1., 0.5, 10.],
         "clip_value": [False, True],
         # "n_minibatch": None,
         # "mini_batch_size": None,
         # "wandb_name": None,
         # "wandb_group": None,
-        "wandb_tags": [["graph-transition", "multi-extrapolation", "fixed-dones", "added cont"]],
+        "wandb_tags": [["graph-transition", "large-output-dim", "added-cont"]],
         # "detect_nan": False,
         "use_wandb": [True],
         "mirror_env": [False],
         "use_valid_env": [True],
-        "output_dim": [1, 4],
+        "output_dim": [24, 256],
         # "fs_coef": [0.0001, 0.001, 0.01, 0.1, 0.5, 1.0, 2.0, 5.0, 10.0],
     }
 
@@ -494,8 +494,8 @@ if __name__ == '__main__':
     parser.add_argument('--max_runs', type=int, default=200)
     parser.add_argument('--hparam_type', type=str, default="train")
 
-    re_use_machine = False
-    specify_host = None#"gpu18"  # "gpu29"#None #"gpu34"
+    re_use_machine = True
+    specify_host = "gpu31"  # "gpu29"#None #"gpu34"
     if specify_host is not None and not re_use_machine:
         print("Warning - specifying host will re-use that host")
 
