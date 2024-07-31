a8ef8022491f2e641af0ead28a203f13eb298245
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-07-31 15:59:11 +0100

    simfin total return under way

--- sfin.py
+++ sfin.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from simfin.names import REVENUE, NET_INCOME
 
 import simfin as sf
@@ -18,6 +19,24 @@ if __name__ == "__main__":
     # Print all Revenue and Net Income for Microsoft (ticker MSFT).
     print(df.loc['MSFT', [REVENUE, NET_INCOME]])
 
+    sf.load_cashflow()
 
+    sp = sf.load_shareprices(variant='daily')
+
+    g = sp.groupby(by='Ticker')
+
+    # sp["date"] = sp.index.get_level_values('Date')
+    # sp["prev_date"] = pd.Series(sp.date).shift(-1)
+
+    sp["Div_adj"] = sp.Close
+    flt = sp.Dividend.isna()
+    sp.Div_adj[flt] = sp.Div_adj[flt] + sp.Dividend[flt]
+    sp.loc[flt, 'Div_adj'] += sp.loc[flt, 'Dividend']
+
+    px_chg_pct = sp.Close.pct_change()
+    adj_px_cng_pct = sp.Div_adj.pct_change()
+    px_chg_pct.loc[flt] = adj_px_cng_pct[flt]
+
+    px_chg_pct.unstack().T
 
     print("something")
