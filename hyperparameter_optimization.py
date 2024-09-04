ea561433b4284e81e370898a9a5782113fb483b3
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-09-04 11:23:28 +0100

    removing entropy from continuous cartpole

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -616,7 +616,7 @@ def graph_ppo_sr_ft():
 
 
 if __name__ == "__main__":
-    cartpole_graph_ppo()
-    # graph_ppo_sr_ft()
+    # cartpole_graph_ppo()
+    graph_ppo_sr_ft()
     # double_graph_symbreg_ft_hparams()
     # pets_graph_transition_cartpole()
