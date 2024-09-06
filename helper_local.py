bb3804e99a36bfeb28909e17f27fa06336d3a5fb
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-09-06 16:25:17 +0100

    sorting out humanoid

--- helper_local.py
+++ helper_local.py
@@ -387,8 +387,8 @@ def initialize_model(device, env, hyperparameters, in_channels=None):
         action_size = action_space.n
         policy = CategoricalPolicy(model, recurrent, action_size, has_vq)
     elif isinstance(action_space, gymnasium.spaces.Box) or isinstance(action_space, gym.spaces.Box):
-        action_size = action_space
-        policy = CategoricalPolicy(model, recurrent, action_size, has_vq, continuous_actions=True)
+        action_size = action_space.n
+        policy = CategoricalPolicy(model, recurrent, action_size*2, has_vq, continuous_actions=True)
     else:
         raise NotImplementedError
     policy.to(device)
