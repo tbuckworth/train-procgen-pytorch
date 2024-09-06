a1fe2feb102aa6d55b7809b6fc7342d51e3bd186
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-09-06 15:51:09 +0100

    sorting out humanoid

--- helper_local.py
+++ helper_local.py
@@ -941,6 +941,8 @@ def get_project(env_name, exp_name):
             project = "Graph Symb Reg"
     elif exp_name == "mountain_car_cont_rew":
         project = "MountainCar Continuous Reward"
+    elif env_name == "humainoid":
+        project = "Humanoid"
     else:
         project = env_name
     if exp_name == "overfit":
