4dae4d0c1404845fcc4b51d54775e3d104e0e75b
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-09-06 16:59:49 +0100

    sorting out humanoid

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -176,7 +176,7 @@ def optimize_hyperparams(bounds,
 
     fh = fixed.copy()
     hparams.update(fh)
-    # run_next(hparams)
+    run_next(hparams)
     try:
         run_next(hparams)
     except Exception as e:
