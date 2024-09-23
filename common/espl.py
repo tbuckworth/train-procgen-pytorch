36c50a40474eb2962846b26a227e49bd340fc0af
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-09-23 11:45:09 +0100

    improved espl understanding

--- common/espl.py
+++ common/espl.py
@@ -2,9 +2,13 @@ import math
 
 import numpy as np
 import torch
+import wandb
 from torch import nn, optim
 from torchinfo import summary
 import matplotlib.pyplot as plt
+
+from helper_local import wandb_login
+from hyperparameter_optimization import init_wandb
 
 op_list = []
 
@@ -199,13 +203,25 @@ class EQL(nn.Module):
     Code from ESPL paper (Efficient Symbolic Policy Learning).
     '''
 
-    def __init__(self, num_inputs, num_outputs, sample_num, hard_gum):
+    def __init__(self, num_inputs, num_outputs, sample_num, hard_gum,
+                 target_ratio=0.001,
+                 spls=0.1,
+                 constrain_scale=1,
+                 l0_scale=0.01,
+                 bl0_scale=0,
+                 target_temp=0.2,
+                 warmup_epoch=0,
+                 hard_epoch=900,
+                 ):
         super(EQL, self).__init__()
-        self.target_ratio = 0.1
-        self.spls = 0.1
-        self.constrain_scale = 1
-        self.l0_scale = 0.01
-        self.bl0_scale = 0
+        self.target_ratio = target_ratio
+        self.spls = spls
+        self.constrain_scale = constrain_scale
+        self.l0_scale = l0_scale
+        self.bl0_scale = bl0_scale
+        self.target_temp = target_temp
+        self.warmup_epoch = warmup_epoch
+        self.hard_epoch = hard_epoch
 
         self.num_inputs = num_inputs
 
@@ -380,28 +396,72 @@ class EQL(nn.Module):
         self.regu_loss = reguloss
         return out.reshape(batch, self.num_outputs)
 
+    def set_temp_target_ratio(self, epoch):
+        self.temp = 1 / ((1 - self.target_temp) * (
+                1 - min(epoch, self.hard_epoch) / self.hard_epoch) + self.target_temp)
+        clip_it = max(min(epoch, self.hard_epoch), self.warmup_epoch)
+        self.target_ratio = self.target_ratio + (1 - self.target_ratio) * (
+                1 - ((clip_it - self.warmup_epoch) / (self.hard_epoch - self.warmup_epoch)) ** 2)
+
 
 if __name__ == "__main__":
-    init_op_list(0)
+    op_list_selection = 0
+    init_op_list(op_list_selection)
     obs_dim = 1
     action_dim = 1
-    epochs = 10000
+    cfg = dict(
+        op_list_selection=op_list_selection,
+        epochs=3000,
+        data_size=1000,
+        lr=1e-3,
+        sample_num=1,
+        hard_gum=True,
+        data_scale=100,
+        target_ratio=0.001,
+        spls=0.1,
+        constrain_scale=1,
+        l0_scale=0.01,
+        bl0_scale=0,
+        target_temp=0.2,
+        warmup_epoch=0,
+        hard_epoch=900,
+    )
+    epochs = cfg["epochs"]
+    data_size = cfg["data_size"]
+    lr = cfg["lr"]
+    sample_num = cfg["sample_num"]
+    hard_gum = cfg["hard_gum"]
+    data_scale = cfg["data_scale"]
 
-    lr = 1e-3
-    sample_num = 1
-    hard_gum = True
     num_inputs = obs_dim
     num_outputs = action_dim
-    model = EQL(num_inputs, num_outputs, sample_num, hard_gum)
+    model = EQL(num_inputs, num_outputs, sample_num, hard_gum,
+                target_ratio=cfg["target_ratio"],
+                spls=cfg["spls"],
+                constrain_scale=cfg["constrain_scale"],
+                l0_scale=cfg["l0_scale"],
+                bl0_scale=cfg["bl0_scale"],
+                target_temp=cfg["target_temp"],
+                warmup_epoch=cfg["warmup_epoch"],
+                hard_epoch=cfg["hard_epoch"],
+                )
     optimizer = optim.Adam(model.parameters(), lr=lr)
     model.sample_sparse_constw(mode=0)
-    obs = (np.random.random((1000, 1)) - .5) * 100
+    obs = (np.random.random((data_size, 1)) - .5) * data_scale
     x = torch.FloatTensor(obs)
     y = x ** 2
+
+    name = np.random.randint(1e5)
+    wandb_login()
+    wb_resume = "allow"  # if args.model_file is None else "must"
+    prefix = "espl"
+    wandb.init(project="espl", config=cfg, sync_tensorboard=True,
+               tags=cfg["wandb_tags"], resume=wb_resume, name=f"{prefix}-{name}")
+
     for epoch in range(epochs):
         y_hat = model.forward(x, mode=1)
 
-        other_loss, sparse_loss, constrain_loss, regu_loss, _, _ = model.get_loss()
+        other_loss, sparse_loss, constrain_loss, regu_loss, l0_loss, bl0_loss = model.get_loss()
         total_loss = nn.MSELoss()(y, y_hat) + other_loss
 
         optimizer.zero_grad()
@@ -409,14 +469,42 @@ if __name__ == "__main__":
         nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.5)
         optimizer.step()
         model.proj()
-        model.temp /= 1.135
-    plt.scatter(y.detach().numpy(), y_hat.detach().numpy())
+        wandb.log({
+            "total_loss": total_loss.item(),
+            "mse_loss": (total_loss - other_loss).item(),
+            "sparse_loss": sparse_loss.item(),
+            "other_loss": other_loss.item(),
+            "constrain_loss": constrain_loss.item(),
+            "regu_loss": regu_loss.item(),
+            "l0_loss": l0_loss.item(),
+            "bl0_loss": bl0_loss.item(),
+            "epoch": epoch,
+            "temp": model.temp,
+        })
+        model.set_temp_target_ratio(epoch)
+    # plt.scatter(y.detach().numpy(), y_hat.detach().numpy())
+    zero_weight_pct = (model.constw == 0).sum() / np.prod(model.constw.shape)
 
     model.sample_sparse_constw(mode=0)
-    y_hat = model.forward(x, mode=0)
-    (model.constw == 0).sum()
+    y_hat_mode_0 = model.forward(x, mode=0)
+    zero_weight_pct_mode_0 = (model.constw == 0).sum() / np.prod(model.constw.shape)
+
+    wandb.log({
+        "y": y.detach().cpu().numpy(),
+        "y_hat": y_hat.detach().cpu().numpy(),
+        "y_hat_mode_0": y_hat_mode_0.detach().cpu().numpy(),
+        "x": obs,
+        "pct_0_weight": zero_weight_pct.item(),
+        "pct_0_weight_mode_0": zero_weight_pct_mode_0.item(),
+    })
+    wandb.finish()
+    exit(0)
 
     plt.scatter(y.detach().numpy(), y_hat.detach().numpy())
+    plt.show()
+
+    plt.scatter(obs, y_hat.detach().numpy())
+    plt.scatter(obs, y.detach().numpy())
     plt.show()
 
     print("done")
---
+++ espl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tfb115
+
+export PATH=/vol/bitbucket/${USER}/train-procgen-pytorch/venvcartpole/bin/:/vol/cuda/12.2.0/bin/:$PATH
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/vol/cuda/12.2.0/lib64:/vol/cuda/12.2.0/lib
+source /vol/bitbucket/${USER}/train-procgen-pytorch/venvcartpole/bin/activate
+. /vol/cuda/12.2.0/setup.sh
+TERM=vt100
+/usr/bin/nvidia-smi
+export CUDA_DIR=/vol/cuda/12.2.0/:${CUDAPATH}
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/12.2.0/
+python3.8 /vol/bitbucket/${USER}/train-procgen-pytorch/espl.py
