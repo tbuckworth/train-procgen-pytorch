a2eac215b2c92415784a356fe76f508affe32817
tbuckworth <55180288+tbuckworth@users.noreply.github.com>
2024-09-23 11:49:26 +0100

    improved espl understanding

--- espl.sh
+++ espl.sh
@@ -11,4 +11,4 @@ TERM=vt100
 /usr/bin/nvidia-smi
 export CUDA_DIR=/vol/cuda/12.2.0/:${CUDAPATH}
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/12.2.0/
-python3.8 /vol/bitbucket/${USER}/train-procgen-pytorch/espl.py
+python3.8 /vol/bitbucket/${USER}/train-procgen-pytorch/common/espl.py
