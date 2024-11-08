93e0931917f55f182d2444d1ee5fd8eed7ead71f
tbuckworth <elkilesh@hotmail.co.uk>
2024-11-08 15:51:07 +0000

    fixed depth issue

--- train.py
+++ train.py
@@ -93,6 +93,8 @@ def train_ppo(args):
         "separate_icm",
         "logsumexp_logits_is_v",
         "entropy_modified",
+        "depth",
+        "mid_weight",
     ]:  # TODO: should this just be all the keys, rather than specify them? why not?
         if var_name in args.__dict__.keys() and args.__dict__[var_name] is not None:
             hyperparameters[var_name] = args.__dict__[var_name]
