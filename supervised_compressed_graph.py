0b9f11385e415e6b947f187b5ee5c36697581d3c
tbuckworth <elkilesh@hotmail.co.uk>
2024-12-03 12:27:01 +0000

    improved supervised compressed graph

--- supervised_compressed_graph.py
+++ supervised_compressed_graph.py
@@ -16,10 +16,11 @@ def main(args):
     n = 10
     k = 2
     m = 5
-    b = int(1000/(m**2))
+    b = int(1000 / (m ** 2))
     lr = 1e-3
     epochs = 1000
     seed = 42
+    ensemble = False
     folder = "test"
     project = "compressed_graph"
     subfolder = "supervised"
@@ -28,7 +29,7 @@ def main(args):
 
     x = torch.rand((b, n)).to(device)
     y = generate_y(x, (b, k)).to(device)
-    x_test = x*2
+    x_test = x * 2
     y_test = generate_y(x_test, (b, k)).to(device)
 
     model = CompressedGraph(in_channels=n,
@@ -38,7 +39,9 @@ def main(args):
                             mid_weight=64,
                             device=device)
     neural_train_loss = train_and_save(model, x, y, epochs, lr, logdir + "/neural_model.pth", nn.MSELoss())
-    neural_test_loss = (y_test-model(x_test)).pow(2).mean()
+    y_hat_neural = model(x)
+    y_test_hat_neural = model(x_test)
+    neural_test_loss = (y_test - y_test_hat_neural).pow(2).mean()
     # Train the pysr bit
     weights = None
     with torch.no_grad():
@@ -46,22 +49,66 @@ def main(args):
     sr_x = flatten_batches_to_numpy(m_in)
     sr_y = flatten_batches_to_numpy(m_out).squeeze()
     msg_model, _ = find_model(sr_x, sr_y, symbdir, save_file, weights, args)
-    # msg_torch = NBatchPySRTorch(msg_model.pytorch(), device)
-    msg_torch = all_pysr_pytorch(msg_model, device)
+    if ensemble:
+        msg_torch = all_pysr_pytorch(msg_model, device)
+        symb_loss = min_batch_loss
+    else:
+        msg_torch = NBatchPySRTorch(msg_model.pytorch(), device)
+        symb_loss = nn.MSELoss()
+
     # Fine tune sr bit:
     model.messenger = msg_torch
-    symb_train_loss = train_and_save(model, x, y, epochs, lr, logdir + "/symb_model.pth", min_batch_loss)
-    symb_test_loss = min_batch_loss(model(x_test), y_test)
+    symb_train_loss = train_and_save(model, x, y, epochs, lr, logdir + "/symb_model.pth", symb_loss)
+    y_hat_symb = model(x)
+    y_test_hat_symb = model(x_test)
+    symb_test_loss = symb_loss(y_test_hat_symb, y_test)
 
     print(f"Neural Train Loss: {neural_train_loss:.4f}\tTest Loss: {neural_test_loss:.4f}")
     print(f"Symbol Train Loss: {symb_train_loss:.4f}\tTest Loss: {symb_test_loss:.4f}")
 
+    if ensemble:
+        elite_idx = min_batch_loss(y_hat_symb, y, True)
+        np_y_train_hat_symb = y_hat_symb[elite_idx].detach().cpu().numpy().reshape(-1)
+        np_y_test_hat_symb = y_test_hat_symb[elite_idx].detach().cpu().numpy().reshape(-1)
+    else:
+        np_y_train_hat_symb = y_hat_symb.detach().cpu().numpy().reshape(-1)
+        np_y_test_hat_symb = y_test_hat_symb.detach().cpu().numpy().reshape(-1)
+
+
+    gold = [y.cpu().numpy().reshape(-1), y_test.cpu().numpy().reshape(-1)]
+    pred = [
+        [y_hat_neural.detach().cpu().numpy().reshape(-1),
+         np_y_train_hat_symb],
+        [y_test_hat_neural.detach().cpu().numpy().reshape(-1),
+         np_y_test_hat_symb],
+    ]
+    titles = [["Neural Train", "Symbolic Train"], ["Neural Test", "Symbolic Test"]]
+
+    fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+    for row in range(2):
+        for col in range(2):
+            axes[row][col].scatter(
+                x=gold[row],
+                y=gold[row],
+                c='black',
+            )
+            axes[row][col].scatter(
+                x=gold[row],
+                y=pred[row][col],
+            )
+            axes[row][col].set_title(titles[row][col])
+    plt.tight_layout()
+    # plt.title()
+    plt.show()
 
     print("done")
 
-def min_batch_loss(y_hat, y):
+
+def min_batch_loss(y_hat, y, return_idx=False):
     y = y.unsqueeze(0)
-    losses = ((y_hat-y)**2).mean(dim=tuple(range(1,y.ndim)))
+    losses = ((y_hat - y) ** 2).mean(dim=tuple(range(1, y.ndim)))
+    if return_idx:
+        return losses.argmin()
     loss = losses.min()
     return loss
 
@@ -84,8 +131,8 @@ def train_and_save(model, x, y, epochs, lr, logdir, loss_fn):
 
 def generate_y(x, shp):
     y = torch.zeros(shp)
-    y[...,0] = x[...,0] * x[..., 4] + torch.sin(x[..., 2])
-    y[...,1] = x[...,1] + torch.cos(x[..., 4]) * torch.exp(x[..., 2])
+    y[..., 0] = x[..., 0] * x[..., 4] + torch.sin(x[..., 2])
+    y[..., 1] = x[..., 1] + torch.cos(x[..., 4]) * torch.exp(x[..., 2])
     return y
 
 
